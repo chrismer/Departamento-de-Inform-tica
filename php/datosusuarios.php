c64513c6d0f050f8f9a9cc6f766bddbba9080929
u608014 <cmereles@teco.com.ar>
2024-02-06 20:51:32 -0300

    se renueva la tabla de usuario a usuarios

--- php/datosusuarios.php
+++ php/datosusuarios.php
@@ -10,14 +10,14 @@ switch ($_GET['accion']) {
     case 'agregar':
         $usuario = mysqli_real_escape_string($conexion, $_POST['nombrenuevo']);
         $password = mysqli_real_escape_string($conexion, $_POST['clave1']);
-        $respuesta = mysqli_query($conexion, "insert into usuario (usuario,password) values ('$usuario','$password')");
+        $respuesta = mysqli_query($conexion, "insert into usuarios (usuario,password) values ('$usuario','$password')");
 
         echo json_encode($respuesta);
 
         break;
     case 'existe':
         $nombre = mysqli_real_escape_string($conexion, $_POST['nombrenuevo']);
-        $respuesta = mysqli_query($conexion, "select usuario from usuario where usuario='$nombre'");
+        $respuesta = mysqli_query($conexion, "select usuario from usuarios where usuario='$nombre'");
 
         $cantidad = mysqli_num_rows($respuesta);
         if($cantidad == 1){
--- php/login.php
+++ php/login.php
@@ -7,7 +7,7 @@ $conexion = regresarConexion();
 $usuario = mysqli_real_escape_string($conexion, $_POST['usuario']);
 $password = mysqli_real_escape_string($conexion, $_POST['password']);
 
-$respuesta = mysqli_query($conexion, "select usuario, password from usuario where usuario='$usuario' and password='$password'");
+$respuesta = mysqli_query($conexion, "select usuario, password from usuarios where usuario='$usuario' and password='$password'");
 
 $cantidad = mysqli_num_rows($respuesta);
 
